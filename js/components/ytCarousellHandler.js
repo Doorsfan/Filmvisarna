03792bd8fd13de65b3d44ca7a99576a78b0bebe9
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-03-11 15:55:29 +0100

    added a fixed defaultsize to slider

--- js/components/ytCarousellHandler.js
+++ js/components/ytCarousellHandler.js
@@ -18,18 +18,20 @@ function renderTrailer(data) {
   let html = $(`<div class="video-slider"></div>`);
 
   data.forEach((movie) => {
-    html.append(`
+    html.append(/*html*/ `
     <div class="slide">
       <iframe class="video" src="https://www.youtube.com/embed/${movie.youtubeTrailers[0]}?ecver=2&enablejsapi=1" frameBorder="0"></iframe>
       </div>
   `);
   });
-  html.append(`
+  html.append(/*html*/ `
   <div class="slide-arrow left"></div>
   <div class="slide-arrow right"></div>
   `);
 
   $('body').prepend(html);
+
+  // return html?
 }
 
 function nextSlide(slides) {
@@ -49,24 +51,28 @@ function previousSlide() {
 $('body').on('click', '.left', function (e) {
   slides = $('.slide');
   numOfSlides = slides.length;
-  onYouTubeIframeAPIReady();
+  onYouTubeIframeAPIReady(slides);
   // stopCurrentVideo(slides);
   previousSlide();
 });
+
 $('body').on('click', '.right', function (e) {
   slides = $('.slide');
   numOfSlides = slides.length;
-  onYouTubeIframeAPIReady();
+  onYouTubeIframeAPIReady(slides);
   // stopCurrentVideo(slides);
   nextSlide(slides);
 });
 
 //youtubeAPI
-function onYouTubeIframeAPIReady() {
-  $('.slide').each(function (index, slide) {
+function onYouTubeIframeAPIReady(slides) {
+  $(slides).each(function (index, slide) {
     // Get the `.video` element inside each `.slide`
     let iframe = $(slide).find('.video')[0];
     // Create a new YT.Player from the iFrame, and store it on the `.slide` DOM object
     slide.video = new YT.Player(iframe);
   });
 }
+
+// testa
+export { previousSlide, nextSlide, onYouTubeIframeAPIReady, renderTrailer };
