9e8ed5dae79e51faf7a2f888623ac87326a10839
Your Name <you@example.com>
2021-03-11 22:43:01 +0100

    Added file footer.js

---
+++ js/components/footer.js
@@ -0,0 +1,13 @@
+export default class Footer {
+  render() {
+    let footer = $(/*html*/ `
+      <footer>
+      <h2>Filmvisarna</h2>
+      <h3>Abborrstigen 17<br>
+      220 20 Lund</h3>
+      <p>Grupp 5, Java21v Plushögskolan Lund ©2020.</p>
+      </footer>
+    `);
+    return footer;
+  }
+}
