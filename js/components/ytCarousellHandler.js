08cc76af7fc929f7e4f2285ff4988e78b3a56fc7
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-03-12 08:46:11 +0100

    MAde a class

--- js/components/ytCarousellHandler.js
+++ js/components/ytCarousellHandler.js
@@ -1,20 +1,16 @@
 // Additionnal code for the slider
 
-let pos = 0;
-let slides;
-let numOfSlides;
+export default class ytSlider{
+pos = 0;
+ slides;
+ numOfSlides;
 fetchYoutubeRef();
 
-async function fetchYoutubeRef() {
-  let movie;
-  await $.getJSON('/json/movies.json', (data) => {
-    movie = data;
-  });
-  renderTrailer(movie);
+async  fetchYoutubeRef() {
+  let movie = await $.getJSON('/json/movies.json', (data) => {});
 }
 
-function renderTrailer(data) {
-  //variable to use
+ renderTrailer(data) {
   let html = $(`<div class="video-slider"></div>`);
 
   data.forEach((movie) => {
@@ -31,27 +27,28 @@ function renderTrailer(data) {
 
   $('body').prepend(html);
 
-  // return html?
 }
 
-function nextSlide(slides) {
+ nextSlide(slides) {
   slides.eq(pos).animate({ left: '-100%' }, 500);
   // "loop"
   pos = pos >= numOfSlides - 1 ? 0 : ++pos;
   slides.eq(pos).css({ left: '100%' }).animate({ left: 0 }, 500);
 }
 
-function previousSlide() {
+ previousSlide() {
   slides.eq(pos).animate({ left: '100%' }, 500);
   // "loop"
   pos = pos == 0 ? numOfSlides - 1 : --pos;
   slides.eq(pos).css({ left: '-100%' }).animate({ left: 0 }, 500);
 }
 
+}
+
 $('body').on('click', '.left', function (e) {
   slides = $('.slide');
   numOfSlides = slides.length;
-  onYouTubeIframeAPIReady(slides);
+  // onYouTubeIframeAPIReady(slides);
   // stopCurrentVideo(slides);
   previousSlide();
 });
@@ -59,20 +56,18 @@ $('body').on('click', '.left', function (e) {
 $('body').on('click', '.right', function (e) {
   slides = $('.slide');
   numOfSlides = slides.length;
-  onYouTubeIframeAPIReady(slides);
+  // onYouTubeIframeAPIReady(slides);
   // stopCurrentVideo(slides);
   nextSlide(slides);
 });
 
 //youtubeAPI
-function onYouTubeIframeAPIReady(slides) {
-  $(slides).each(function (index, slide) {
-    // Get the `.video` element inside each `.slide`
-    let iframe = $(slide).find('.video')[0];
-    // Create a new YT.Player from the iFrame, and store it on the `.slide` DOM object
-    slide.video = new YT.Player(iframe);
-  });
-}
+// function onYouTubeIframeAPIReady(slides) {
+//   $(slides).each(function (index, slide) {
+//     // Get the `.video` element inside each `.slide`
+//     let iframe = $(slide).find('.video')[0];
+//     // Create a new YT.Player from the iFrame, and store it on the `.slide` DOM object
+//     slide.video = new YT.Player(iframe);
+//   });
 
-// testa
-export { previousSlide, nextSlide, onYouTubeIframeAPIReady, renderTrailer };
+
