f0b585b8532c00b96516e395f633c06977bf3c2b
WanjaMW <wanjamaria.w@gmail.com>
2021-03-12 17:54:08 +0100

    Added padding, deleted text

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -3,8 +3,6 @@ export default class Footer {
     let footer = $(/*html*/ `
       <footer>
       <h2>Filmvisarna</h2>
-      <h3>Abborrstigen 17<br>
-      221 20 Småstad</h3>
       <p>Grupp 5, Java21v Plushögskolan Lund ©2021.</p>
       </footer>
     `);
