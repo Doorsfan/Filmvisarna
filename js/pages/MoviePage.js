9d9589904765b8039a0dd2f83297eb8cd818f13c
Martin-Persson <jm-persson@hotmail.com>
2021-03-13 23:44:33 +0100

    Rough layout + made som changes in movies.json

--- js/pages/MoviePage.js
+++ js/pages/MoviePage.js
@@ -17,9 +17,10 @@ export default class MoviePage {
           </figure>
           <div class="movie-text">
             <h2 class="title-name"><p>${data.title}</p></h2>
-            <h4>Genre:</h4> <p>${data.genre}</p>
-            <h4>Speltid:</h4> <p>${data.length + ' minuter'}</p>
-          </div><hr>
+            <h4>Genre:&nbsp;</h4> <p>${data.genre} &emsp;</p>
+            <h4>Speltid:&nbsp;</h4> <p>${data.length + ' minuter'}</p>
+            <h4>Handling:&nbsp;</h4> ${data.description}
+          </div>
         </section>`;
     });
 
