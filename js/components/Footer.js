845fafb81182fe6fe9d851eba4335925ad8880ae
WanjaMW <wanjamaria.w@gmail.com>
2021-03-13 01:39:16 +0100

    Deleted text frpm footer

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -2,7 +2,6 @@ export default class Footer {
   render() {
     let footer = $(/*html*/ `
       <footer>
-      <h2>Filmvisarna</h2>
       <p>Grupp 5, Java21v Plushögskolan Lund ©2021.</p>
       </footer>
     `);
