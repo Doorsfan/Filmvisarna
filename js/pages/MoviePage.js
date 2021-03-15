d403837426291f939ef165ca9218dc0f9de357bc
Martin-Persson <jm-persson@hotmail.com>
2021-03-15 12:34:18 +0100

    Added two dropdown menues

--- js/pages/MoviePage.js
+++ js/pages/MoviePage.js
@@ -9,7 +9,19 @@ export default class MoviePage {
     if (!this.movies) {
       await this.read();
     }
-    let html = '<div class="movie-container"><h1>Våra filmer</h1>';
+    let html = /*html*/ `<div class="movie-container"><h1>Våra filmer</h1><div class="movie-filter">
+    <select class="category-filter"><option>Kategori</option>
+    <option>drama</option>
+    <option>brott</option>
+    <option>komedi</option>
+    </select>
+    <select class="age-filter"><option>Åldersgrupp</option>
+    <option>-7</option>
+    <option>7-15</option>
+    <option>15+</option>
+    </select>
+    </div>`;
+
     this.movies.forEach((data) => {
       html += /*html*/ `<section class="movie-info">
           <div class="movie-poster">
