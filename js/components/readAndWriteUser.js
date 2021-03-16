a66af63408a7e96b81ccc1467596303abe988593
Mikael Rusin <mikael.rusin@gmail.com>
2021-03-16 15:14:08 +0100

    Modified to use ._load instead
    After discussions regarding the code - we agreed to revert back to utilizing ._load - since i had misunderstood how to await the asynch structure of the ._load function

--- js/components/readAndWriteUser.js
+++ js/components/readAndWriteUser.js
@@ -4,8 +4,16 @@ export default class readAndWriteUser {
   }
 
   async loadUser(username) {
+    /*
     fetch("/json/users/" + username + ".json")
       .then(response => response.json())
-      .then(user => console.log("Users password was: " + user.password));
+      .then(user => {
+        this.blyat = user.password;
+      }); */
+    this.user = await JSON._load("/users/" + username + ".json");
+    if (!this.user) {
+      await this.loadUser("example.user@someEmail.se");
+    }
+    return this.user;
   }
 }
