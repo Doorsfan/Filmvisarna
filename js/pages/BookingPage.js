958e66d13f5d5e17fd4d219c38a18f764af9d810
WanjaMW <wanjamaria.w@gmail.com>
2021-03-17 16:02:29 +0100

    Rendered shows for booking page, corrected spelling in titles in movies.json and movieSchedule.json

--- js/pages/BookingPage.js
+++ js/pages/BookingPage.js
@@ -1,28 +1,38 @@
-/*  Min task: Boka-Film-Page) Hämta Salong, längd, kategori tidslag från json och rendera mha jquery
-*   Hämtas från movieSchedule.json: auditorium, time.
-*   Hämtas från movies.json:  title, length, genre.
-*
-*   Daniels task: US 2) Rendera boka-film-page mha jquery
-*/
+export default class BookingPage  {
 
-export default class BookingPage
-{
-  render() {
-    let html = $(/*html*/`  
+  async readShowsFromJson() {
+    this.shows = await $.getJSON('json/movieSchedule.json');
+    this.movies = await $.getJSON('json/movies.json');
+  }
+
+  async render() {
+    let html = `
     <div class = "bookingpage-container">
     <div class = "bookingpage-cover"></div>
     <h1>Visningar</h1>
+    `;
+    if (!this.shows) {
+      await this.readShowsFromJson();
+    }
+    for (let show of this.shows) {
+      let movie = this.movies.find(movie => movie.title === show.film);
+      if (!movie) {
+        console.warn('unable to find movie with title', show.film);
+        continue;
+      }
+
+      html += `
     <div class = "bookingpage-show">
-    <button>Boka biljett</button></div>
-    </div> `)
-    return html
-  }
-
-  async readShowsFromJson() {
-    this.shows = await $.getJSON('movieSchedule.json');
-    this.movies = await $.getJSON('movies.json');
-
-
+      <img src=${movie.images[0]}>
+      <h2>${show.film}</h2>
+      <p>${movie.genre} | ${movie.length} minuter</p>
+      <h3>${show.time} | ${show.auditorium}</h3>
+      <button>Boka biljett</button>
+      </div>
+      `;
+    }
+    html += '</div>';
+    return html;
   }
 
   
