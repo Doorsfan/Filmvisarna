1967ad093aa775a0c5eef126c63b0bbb26befe5d
Martin-Persson <jm-persson@hotmail.com>
2021-03-18 21:50:36 +0100

    removed last comma when page rerenders

--- js/components/filterMovies.js
+++ js/components/filterMovies.js
@@ -1,3 +1,5 @@
+import MoviePage from '../pages/MoviePage.js';
+
 export default class filterMovies {
   async reRenderMovies(category, age) {
     this.movies = await $.getJSON('/json/movies.json');
@@ -16,15 +18,18 @@ export default class filterMovies {
         filteredMovies.push(movie);
       }
     });
+
     filteredMovies.forEach((data) => {
+      let genreString = '';
+      genreString = this.removingUnwantedLastComma(data, genreString);
       html += /*html*/ `<section class="movie-info">
           <div class="movie-poster">
             <a href="#aboutPage${data.id}"><img src="${data.images[0]}"></a>
           </div>
           <div class="movie-text">
             <h2 class="title-name"><p>${data.title}</p></h2>
-            <div class="genre"><h4>Genre:&nbsp;</h4> <p>${data.genre}</p></div>
-            <div class="runtime"><h4>Speltid:&nbsp;</h4> <p>${
+            <div class="genre"><h4>Genre: </h4> <p>${genreString}</p></div>
+            <div class="runtime"><h4>Speltid: </h4> <p>${
               data.length + ' minuter'
             }</p></div>
             <div class="story"><h4>Handling:&nbsp;</h4> ${
@@ -36,5 +41,19 @@ export default class filterMovies {
     html += '</div></div>';
     $('.movies-main-box').html(' ');
     $('.movies-main-box').append(html);
+  }
+  removingUnwantedLastComma(data, genreString) {
+    if (Array.isArray(data.genre)) {
+      for (let i = 0; i < data.genre.length; i++) {
+        if (i === data.genre.length - 1) {
+          genreString += data.genre[i];
+        } else {
+          genreString += data.genre[i] + ', ';
+        }
+      }
+    } else {
+      genreString = data.genre;
+    }
+    return genreString;
   }
 }
