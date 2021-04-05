afdb97f7f09ef1b16aed4f48e123c65f176e6389
Danne <daniellangas91@gmail.com>
2021-04-05 17:10:01 +0200

    Added Icons and styled footer

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -5,17 +5,27 @@ export default class Footer {
       <div class="footer-container">
         <div class="footer-column">
           <p class="footer-title">Social Media</p>
-          <a href="#"class="footer-link">Twitter</a>
-          <a href="#"class="footer-link">Instagram</a>
-          <a href="#"class="footer-link">Facebook</a>
+            <div class="footer-content">
+              <a href="#"class="footer-link">Twitter <img src="../../images/twitter_icon.png"/></a>
+              <a href="#"class="footer-link link-insta">Instagram<img src="../../images/insta_icon.png"/></a>
+              <a href="#"class="footer-link link-fb">Facebook<img src="../../images/facebook_icon.webp"/></a>
+            </div>
         </div>
         <div class="footer-column">
+        <div></div>
           <p class="footer-title">Gäster</p>
           <a href="#"class="footer-link">Boka film</a>
+          <a href="#"class="footer-link">Frågor och svar</a>
+          <a href="#"class="footer-link">Feedback</a>
+          <a href="#"class="footer-link">Vårt mål</a>
         </div>
         <div class="footer-column">
+        <div></div>
           <p class="footer-title">Medlemmar</p>
           <a href="#"class="footer-link">Logga in</a>
+          <a href="#"class="footer-link">Bli medlem</a>
+          <a href="#"class="footer-link">Nyheter</a>
+          <a href="#"class="footer-link">Frågor</a>
         </div>
       </div>
       <p class="footer-group">Grupp 5, Java21v Plushögskolan Lund ©2021.</p>      
