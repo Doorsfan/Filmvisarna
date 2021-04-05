9b5e00153a8b6aed888827506078ed4cddc03eba
Danne <daniellangas91@gmail.com>
2021-04-05 14:57:24 +0200

    Continued with setting height and width for elements

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -4,13 +4,18 @@ export default class Footer {
       <footer>
       <div class="footer-container">
         <div class="footer-column">
-          <p>TEST</p>
+          <p class="footer-title">Social Media</p>
+          <a href="#"class="footer-link">Twitter</a>
+          <a href="#"class="footer-link">Instagram</a>
+          <a href="#"class="footer-link">Facebook</a>
         </div>
         <div class="footer-column">
-          <p>TEST</p>
+          <p class="footer-title">Gäster</p>
+          <a href="#"class="footer-link">Boka film</a>
         </div>
         <div class="footer-column">
-          <p>TEST</p>
+          <p class="footer-title">Medlemmar</p>
+          <a href="#"class="footer-link">Logga in</a>
         </div>
       </div>
       <p class="footer-group">Grupp 5, Java21v Plushögskolan Lund ©2021.</p>      
