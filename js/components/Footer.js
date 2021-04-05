f366218330c9db635acdede6e94b86d1002a0dde
Danne <daniellangas91@gmail.com>
2021-04-05 14:22:22 +0200

    changed height and width and added color of footer

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -2,7 +2,18 @@ export default class Footer {
   render() {
     let footer = $(/*html*/ `
       <footer>
-      <p>Grupp 5, Java21v Plushögskolan Lund ©2021.</p>
+      <div class="footer-container">
+        <div class="footer-column">
+          <p>TEST</p>
+        </div>
+        <div class="footer-column">
+          <p>TEST</p>
+        </div>
+        <div class="footer-column">
+          <p>TEST</p>
+        </div>
+      </div>
+      <p class="footer-group">Grupp 5, Java21v Plushögskolan Lund ©2021.</p>      
       </footer>
     `);
     return footer;
