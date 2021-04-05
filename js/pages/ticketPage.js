616c2b39ba8a55ec65c8a561dc5b3091afe8de89
Danne <daniellangas91@gmail.com>
2021-04-05 09:03:09 +0200

    resolved merge

--- js/pages/ticketPage.js
+++ js/pages/ticketPage.js
@@ -8,69 +8,6 @@ export default class TicketPage {
     this.eventHandler();
   }
 
-<<<<<<< HEAD
-  getRow() {
-    let seatInfo = [];
-    this.auditorium.forEach((_, index) => {
-      if (
-        this.auditorium[index].auditorium === window.selectedShow['auditorium']
-      ) {
-        let chosenAuditorium = this.auditorium[index];
-        let totalSeats = 0;
-
-        window.selectedShow['seat'].forEach((_, seatIndex) => {
-          totalSeats = 0;
-          let found = false;
-          chosenAuditorium.seatsPerRow.forEach((_, index) => {
-            totalSeats += chosenAuditorium.seatsPerRow[index];
-            if (
-              totalSeats >= window.selectedShow['seat'][seatIndex] &&
-              !found
-            ) {
-              let seat = window.selectedShow['seat'][seatIndex];
-              let row = index + 1;
-              seatInfo.push({ seat, row });
-              found = true;
-            }
-          });
-        });
-      }
-    });
-    return seatInfo;
-  }
-
-  popModal() {
-    let modalInfo = {
-      username: window.username ? window.username : 'Ej Användare',
-      film: window.selectedShow.film,
-      show:
-        window.selectedShow.auditorium +
-        ' | ' +
-        window.selectedShow.date +
-        ' | ' +
-        window.selectedShow.time,
-      seats: this.getRow(),
-    };
-
-    this.modal = new Modal().render(modalInfo);
-    $('.ticketpage-container').append(this.modal);
-  }
-
-  async render() {
-    if (!this.saloonView) {
-      this.saloonView = await new Saloon().render();
-      this.auditorium = await $.getJSON('./json/auditoriums.json');
-    }
-
-    $('main').on('click', '.ticket-booking', () => {
-      window.username
-        ? (window.selectedShow.id = window.username)
-        : (window.selectedShow.id = 'none');
-      new ReadNWrite().saveBookings(window.selectedShow, window.username);
-      let string = JSON.stringify(window.selectedShow);
-      this.popModal();
-    });
-=======
   eventHandler() {
     let bookedBefore = JSON.parse(sessionStorage.getItem('selectedShow'));
     $('main').on('click', '.ticket-booking', () => this.saveUserBooking());
@@ -137,7 +74,6 @@ export default class TicketPage {
     this.saloonView = await new Saloon().render();
     this.movieSchedule = await JSON._load('movieSchedule.json');
 
->>>>>>> main
     return /*html*/ ` 
     <div class='ticketpage-container'>
       <div class="ticketpage-content">
