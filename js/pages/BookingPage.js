30f18026d7231f7b9f577ac4a2b7c2f520b88dc0
WanjaMW <80105300+WanjaMW@users.noreply.github.com>
2021-04-08 07:10:14 +0200

    Date fix and cleanup in bookingPage

--- js/pages/BookingPage.js
+++ js/pages/BookingPage.js
@@ -1,41 +1,20 @@
+import enrichScheduleWithEmptySeats from '../components/EmptySeats.js';
 export default class BookingPage {
-  showDate = new Date().toISOString().split('T')[0];
+
+  nowDate = new Date().toLocaleDateString('sv-SE');
+  showDate = this.nowDate;
 
   constructor() {
     let that = this;
-    $('body').on('click', '.datePicker', function () {
-      //Assign the previousDate to be able to default to the previous Date
-      that.previousDate = $(this).val();
-    })
     $('body').on('change', '.datePicker', async function () {
-      if ($(this).val() >= new Date().toISOString().slice(0, 10)) {
-        //If the showDate chosen is valid, change the showDate and re-render the update
-        that.showDate = $(this).val();
-        $('main').html(await that.render());
-      } else {
-        //If not, just re-assign the value of the date picker to be the previous date
-        $(this).val(that.previousDate);
-        //And then display the Modal
-        $('main').prepend(`
-      <div class="BookingPage BookingModal">
-        <div class="BookingPage modal-content">
-          <span class="BookingPage closeFailedBookingModal">&times;</span>
-          <p>Du kan ej boka ett datum som redan passerat!</p>
-        </div>
-      </div>`);
-      }
+      that.showDate = $(this).val();
+      $('main').html(await that.render());
     });
     this.eventHandler();
-    this.addCloseModalHandler();
   }
 
-  addCloseModalHandler() {
-    $('main').on('click', '.BookingPage.closeFailedBookingModal', (event) => {
-      $('.BookingModal').remove();
-    });
-  }
   async readShowsFromJson() {
-    this.shows = await $.getJSON('json/movieSchedule.json');
+    this.shows = await enrichScheduleWithEmptySeats(await $.getJSON('json/movieSchedule.json'));
     this.movies = await $.getJSON('json/movies.json');
   }
 
@@ -43,7 +22,7 @@ export default class BookingPage {
     let html = `
     <div class = "bookingpage-container">
     <div class = "bookingpage-cover"></div>
-    <label class="datePickerLabel">Visningsdatum: <input class="datePicker" type="date" value="${this.showDate}"></label>
+    <label class="datePickerLabel">Visningsdatum: <input class="datePicker" type="date" min="${this.nowDate}" value="${this.showDate}"></label>
     `;
     if (!this.shows) {
       await this.readShowsFromJson();
@@ -55,15 +34,19 @@ export default class BookingPage {
         console.warn('unable to find movie with title', show.film);
         continue;
       }
+      
       let purgedName = movie.title.replaceAll(/\s/g, '');
       purgedName = purgedName.replaceAll("'", '');
+
       html += `
     <div class = "bookingpage-show">
-      <a href="#aboutPage/${movie.id}"><img src=${movie.images[0]}></a>
+      <a href="#aboutPage/${movie.id}"><img alt=${movie.title} src=${movie.images[0]}></a>
       <h2><a href="#aboutPage/${movie.id}">${show.film}</a></h2>
-      <p>${movie.genre.join ? movie.genre.join(', ') : movie.genre}
-      | ${movie.ageRating === "barntillåten" ? "Barntillåten" : movie.ageRating + " år"} | ${Math.floor(movie.length / 60)} tim ${movie.length % 60} min</p>
-      <h3>${show.date} | ${show.time} | ${show.auditorium}</h3>
+      <p>${movie.genre.join ? movie.genre.join(', ') : movie.genre} | 
+      ${movie.ageRating === "barntillåten" ? "Barntillåten" : movie.ageRating + " år"} | 
+      ${Math.floor(movie.length / 60)} tim ${movie.length % 60} min</p>
+      <div class="show-info">${show.date} | ${show.time} | ${show.auditorium}</div>
+      <p>Lediga platser: ${show.emptySeats}</p>
       <a href="#ticketPage"><button class="bookingpage${purgedName} &${
         show.date
       } ${show.time}& ${show.auditorium} ${
